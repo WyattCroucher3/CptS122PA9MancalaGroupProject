ec508b8f30a6cafd9e79614a4dacc11d255942f5
WyattCroucher3 <83088706+WyattCroucher3@users.noreply.github.com>
2021-04-25 15:29:42 -0700

    Began making a layout for the algorithm of the program

--- GameBoard.cpp
+++ GameBoard.cpp
@@ -2,18 +2,5 @@
 
 GameBoard::GameBoard()
 {
-	gameBoard[0] = new BoardPocket;
-	gameBoard[1] = new BoardPocket;
-	gameBoard[2] = new BoardPocket;
-	this->gameBoard[3] = new BoardPocket;
-	this->gameBoard[4] = new BoardPocket;
-	this->gameBoard[5] = new BoardPocket;
-	this->gameBoard[6] = new MancalaPocket;
-	this->gameBoard[7] = new BoardPocket;
-	this->gameBoard[8] = new BoardPocket;
-	this->gameBoard[9] = new BoardPocket;
-	this->gameBoard[10] = new BoardPocket;
-	this->gameBoard[11] = new BoardPocket;
-	this->gameBoard[12] = new BoardPocket;
-	this->gameBoard[13] = new MancalaPocket;
+
 }
