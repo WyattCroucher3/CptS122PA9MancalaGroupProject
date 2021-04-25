ca1d77a93aa8ca73d47f0e68b77bacd989378b36
WyattCroucher3 <83088706+WyattCroucher3@users.noreply.github.com>
2021-04-25 12:36:29 -0700

    Add files via upload

---
+++ Pocket.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::fstream;
+using std::string;
+using std::vector;
+
+class Pocket
+{
+public:
+
+
+
+
+private:
+	int beads;
+};
